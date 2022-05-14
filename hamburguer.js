add3ded1180950a1af6fe1e752f7d68254a1f358
Kevin Presley <kevinpresley1995@gmail.com>
2022-05-13 21:21:18 -0300

    banco de dados ficticio

---
+++ hamburguer.js
@@ -0,0 +1,10 @@
+let hamburgerjason = [
+{id:1, name:'X-Salada', img:'', price:12.99, sizes:['90g', '120', '200g'], description:'Pão,Hamburguer,Queijo,Salada'},
+{id:2, name:'X-Bacon', img:'', price:12.99, sizes:['90g', '120', '200g'], description:'Pão,Hamburguer,Queijo,Bacon'},
+{id:3, name:'X-Egg', img:'', price:12.99, sizes:['90g', '120', '200g'], description:'Pão,Hamburguer,Queijo,Ovo'},
+{id:4, name:'X-Tudo', img:'', price:15.99, sizes:['90g', '120', '200g'], description:'Pão,Hamburguer,Queijo,Salada,Bacon,ovo'},
+{id:5, name:'Especial', img:'', price:20, sizes:['90g', '120', '200g'], description:'Pão,Hamburguer,Queijo,Salada,Bacon,Cheddar,Molho especial'},
+{id:6, name:'Fome Zero', img:'', price:23.99, sizes:['90g', '120', '200g'], description:'Pão,Hamburguer,Queijo,Salada,Bacon,Lombo,Frango,Maionese'},
+{id:7, name:'Especial Duplo', img:'', price:25, sizes:['90g', '120', '200g'], description:'Pão,2xHamburguer,Queijo,Salada,Bacon,Cheddar,Molho especial'},
+{id:8, name:'Tio Vegano', img:'', price:16, sizes:['90g', '120', '200g'], description:'Pão,hamburguer de berinjela,e condimentos Veganos'},
+];
