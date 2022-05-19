ac95327340632560f337ec8e922d57bf230bb100
Kevin Presley <kevinpresley1995@gmail.com>
2022-05-19 18:55:03 -0300

    adcionando decriçao

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const c = (el)=> {
-    return document.querySelector(el);
-} 
+const c = (el)=> document.querySelector(el);
+const cl = (el)=> document.querySelectorAll(el);
 
-hamburguerjason.map((intem, index)=> {
+
+hamburguerjason.map((item, index)=> {
     let hambitem = c('.models .hamb-item').cloneNode(true);//criando função hambitem para facilitar a leitura do codigo 
     //preencher informaçoes em hambitens
+    hambitem.querySelector('.hamb-item--name').innerHTML = item.name;
+    hambitem.querySelector('.hamb-item--description').innerHTML = item.description;
+    hambitem.querySelector('.hamb-item--name').innerHTML = item.name;
+
+
+
     c('.hamb--area').append(hambitem);// 'append'adiciona itens ao invés de substituir
 });
