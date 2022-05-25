e1daf9b01a09bada4fbff71e6b0be865e3e6460b
Kevin Presley <kevinpresley1995@gmail.com>
2022-05-25 16:51:25 -0300

    corrigindo organozaçao do codigo

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const cl = (el)=> document.querySelectorAll(el);
 hamburguerjason.map((item, index)=> {
     let hambitem = c('.models .hamb-item').cloneNode(true);//criando função hambitem para facilitar a leitura do codigo 
     //preencher informaçoes em hambitens
+    
+    hambitem.querySelector('.hamb-item--image img').src = item.img;
+    hambitem.querySelector('.hamb-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     hambitem.querySelector('.hamb-item--name').innerHTML = item.name;
     hambitem.querySelector('.hamb-item--description').innerHTML = item.description;
-    hambitem.querySelector('.hamb-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
-    hambitem.querySelector('.hamb-item--image img').src = item.img;
+
 
     c('.hamb--area').append(hambitem);// 'append'adiciona itens ao invés de substituir
 });
